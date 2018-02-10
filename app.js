27d353a8fde856d42ff04c6efeb1f277997ed35a
blazec <blaise.calaycay@gmail.com>
2018-02-10 17:52:34 -0500

    Added db config

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-var express = require('express');
-var pg = require('pg');
-// var conString = "postgres://localhost:5432/report";
-//
-// var client = new pg.Client(conString);
-// client.connect();
+const express = require('express');
+const pg = require('pg');
+const dbConfig = require('./db-config.json');
 
-var app = express();
+const dbUser = dbConfig["user"]
+const dbPassword = dbConfig["password"]
+const dbHost = dbConfig["host"]
+const dbPort = dbConfig["port"]
+const dbName = dbConfig["database"]
+
+const conString = 'postgres://'
+                  + dbUser + ":"
+                  + dbPassword + "@"
+                  + dbHost + ":"
+                  + dbPort + "/"
+                  + dbName;
+
+const client = new pg.Client(conString);
+client.connect();
+
+const app = express();
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello Viktorija!');
+  client.query('INSERT INTO test(id) VALUES(4)');
 });
+
+app.get('/location', function(req, res) {
+  res.send('Location');
+})
+
+
 app.listen(8080, function () {
   console.log('Port reportapp listening on port 8080!');
 });
