b9d7bbeb3be1298e562b9dbecf91647854d2c30a
blazec <>
2018-02-10 16:47:25 -0500

    Added node-postgres as a dependency

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 var express = require('express');
+var pg = require('pg');
+// var conString = "postgres://localhost:5432/report";
+//
+// var client = new pg.Client(conString);
+// client.connect();
+
 var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
