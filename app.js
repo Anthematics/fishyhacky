40cdfa61c01cfeba8740688bab01f5534e7c68c8
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2018-02-10 23:10:00 -0500

    implemented post end point

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const pg = require('pg');
 const dbConfig = require('./db-config.json');
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 const dbUser = dbConfig["user"]
 const dbPassword = dbConfig["password"]
@@ -18,7 +23,7 @@ const conString = 'postgres://' +
 const client = new pg.Client(conString);
 client.connect();
 
-const app = express();
+
 
 app.get('/', (req, res) => {
   res.send('Hello Viktorija!');
@@ -30,9 +35,7 @@ app.get('/locations/:id', (req, res) => {
                 'WHERE vessel_id = $1';
   const values = [req.params.id];
 
-
-
-  client.query(queryString, values, (err, result) => {
+    client.query(queryString, values, (err, result) => {
     rows = result.rows;
     locations = [];
     for (var i = 0; i < rows.length; i++) {
@@ -47,6 +50,29 @@ app.get('/locations/:id', (req, res) => {
 
   })
 })
+
+app.post('/locations',function(req,res){
+   const id = req.body.id;
+   const locations = req.body.locations;
+
+   console.log(id);
+   console.log(locations);
+
+
+   for (var i = 0; i < locations.length; i++) {
+    const queryString = 'INSERT into vessel_location (vessel_id, timestamp, latitude, longitude) VALUES($1, $2, $3, $4)'
+    const values = [id, locations[i].timestamp, locations[i].latitude, locations[i].longitude];
+
+      client.query( queryString, values,
+
+          function(err, result) {
+              if (err) {
+                  console.log(err);
+              }
+
+          });
+    }
+});
 
 app.listen(8080, function () {
   console.log('Port reportapp listening on port 8080!');
